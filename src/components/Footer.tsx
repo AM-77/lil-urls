e7cdbdb83629983caf336bc0b0d3bd2fa89d7168
AM-77 <amine.griche77@protonmail.ch>
2020-07-04 17:14:14 +0200

    add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>Made with <span>&lt;/&gt;</span> by <a href="https://www.github.com/AM-77">Med Amine</a> at <span>AM-77</span></p>
+    </div>
+  )
+}
